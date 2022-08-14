6c4e6aeeaccb95cecb6a657601a22afb926f08fd
Kas S <82743976+Kas-S@users.noreply.github.com>
2022-08-14 13:26:45 +0500

    Generating all subsets using bitmasks

---
+++ generating_subsets.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<vector<int>> subsets;
+  for (int mask = 0; mask < (1 << n); mask++) {
+    vector<int> subset;
+    for(int i = 0; i < n; ++) {
+      if (mask & (1 << i)) subset.push_back(i);
+    }
+    subsets.push_back(subset);
+  }
+}
